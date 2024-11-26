ab6e43dff3d16fada2533297519740cd7e4ed779
Mani Ramanagopal <{ID}+{username}@users.noreply.github.com>
2024-11-26 13:27:09 -0500

    tested boson wrapper

--- cameras/wrapper_boson.py
+++ cameras/wrapper_boson.py
@@ -1,6 +1,8 @@
 import cv2
 import time
 import logging
+import numpy as np
+
 from flirpy.camera.threadedboson import ThreadedBoson
 
 
@@ -10,10 +12,13 @@ class BosonWithTelemetry(ThreadedBoson):
 
         self.configure()
         self.start()
-        self.camera.do_ffc()        
+        self.camera.do_ffc()    
+
+    def __del__(self):
+        self.stop()    
 
     def compute_timestamp_offset(self):
-        latest_image, system_time = self.camera.grab(), time.time()
+        (_, latest_image), system_time = self.camera.cap.read(), time.time()
         _, cam_timestamp = self.parse_telemetry(latest_image[:2, :])
         self.timestamp_offset = system_time - cam_timestamp
 
@@ -41,3 +46,25 @@ class BosonWithTelemetry(ThreadedBoson):
         timestamp_in_ms = telemetry[0, 140] * 2**16 + telemetry[0, 141]
         timestamp = timestamp_in_ms / 1000.0
         return frame_counter, timestamp
+
+
+if __name__ == "__main__":
+    
+    boson = BosonWithTelemetry()
+    
+    cv2.namedWindow("Boson", cv2.WINDOW_NORMAL)
+    
+    while True:
+        image, timestamp = boson.get_next_image()
+
+        image = cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX)
+        image = np.uint8(image)
+        print(f"Timestamp: {timestamp}\r", end="")
+
+        cv2.imshow("Boson", image)
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+    
+    boson.stop()
+    cv2.destroyAllWindows()
